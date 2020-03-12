43f3f33bca007dfffcd08d8b96d9a8239e917bb4
hontarenkoYana <yana.hontarenko@gmail.com>
2020-03-13 01:09:20 +0200

    Add routes and test for them

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from calculator import Calculator
+from flask import Flask, request, render_template
+import logging
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!\n'
+def main():
+    return render_template("index.html")
+
+@app.route('/addition', methods=["POST"])
+def add():
+    first_term = int(request.form['first_term'])
+    second_term = int(request.form['second_term'])
+    return render_template("index.html", result=Calculator().add(first_term, second_term))
+
+@app.route('/substraction', methods=["POST"])
+def substraction():
+    first_term = int(request.form['first_term'])
+    second_term = int(request.form['second_term'])
+    return render_template("index.html", result=Calculator().subtract(first_term, second_term))
+
+@app.route('/multiplication', methods=["POST"])
+def multiplication():
+    first_term = int(request.form['first_term'])
+    second_term = int(request.form['second_term'])
+    return render_template("index.html", result=Calculator().multiplication(first_term, second_term))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)     # open for everyone
--- test_app.py
+++ test_app.py
@@ -11,7 +11,24 @@ class TestCalculator(unittest.TestCase):
         app.app.testing = True
         self.app = app.app.test_client()
 
-    def test_hello(self):
+    def test_main(self):
         rv = self.app.get('/')
+        self.assertEqual('200 OK', rv.status)
+
+    def test_addition(self):
+        params = {"first_term": 2, "second_term": 3}
+        rv = self.app.post('/addition', data=params)
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        self.assertIn("5", rv.data.decode("utf-8"))
+
+    def test_substraction(self):
+        params = {"first_term": 4, "second_term": 2}
+        rv = self.app.post('/substraction', data=params)
+        self.assertEqual(rv.status, '200 OK')
+        self.assertIn("2", rv.data.decode("utf-8"))
+
+    def test_multiplication(self):
+        params = {"first_term": 3, "second_term": 3}
+        rv = self.app.post('/multiplication', data=params)
+        self.assertEqual(rv.status, '200 OK')
+        self.assertIn("9", rv.data.decode("utf-8"))
