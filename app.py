e693bfa5bca20738b1d7adbeabc217002abe673f
hontarenkoYana <yana.hontarenko@gmail.com>
2020-03-12 23:58:38 +0200

    change port

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello_world():
     return 'Hello World!\n'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')     # open for everyone
+    app.run(host='0.0.0.0', port=5000)     # open for everyone
