3d33ffdfe5db6a2d64c9073e4d1b479bc59b30ea
hontarenkoYana <yana.hontarenko@gmail.com>
2020-03-14 15:47:21 +0200

    Fix adding

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ Calculator library containing basic math operations.
 class Calculator:
     @staticmethod
     def add(first_term, second_term):
-        return 33
+        return first_term + second_term
 
     @staticmethod
     def subtract(first_term, second_term):
